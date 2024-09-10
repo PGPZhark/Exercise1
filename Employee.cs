a5e7853914a950b8c5c4b6221f4801086bb6c6e8
Zhark1561 <Simon.Haglund@futuregames.nu>
2024-09-10 15:01:41 +0200

    potatis

---
+++ Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1
+{
+    public class Employee
+    {
+        public string name;
+        public float wage;
+
+        
+
+        public Employee(string name, float wage)
+        {
+            this.name = name;
+            this.wage = wage;
+        }
+
+        public void PrintEmployeeDetails()
+        {
+            Console.WriteLine($"Name: {name}, Wage: {wage}kr/h");
+        }
+
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,11 +1,108 @@
-﻿namespace Exercise1
+﻿using System.Collections;
+using System.Runtime.CompilerServices;
+
+namespace Exercise1
 {
     internal class Program
     {
-        static void Main(string[] args)
+
+
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
-            Console.ReadLine();
+            List<Employee> employees = new List<Employee>();
+            bool runProgram = true;
+
+
+            do
+            {
+                string command;
+                Console.WriteLine(" Commands ");
+                Console.WriteLine("1: Register employee");
+                Console.WriteLine("2: View all employees");
+                Console.WriteLine("9: Quit application");
+                Console.Write("Your command: ");
+                command = Console.ReadLine();
+
+                switch (command)
+                {
+                    case "1":
+                        AddEmployee();
+                        break;
+                    case "2":
+                        Console.WriteLine(" ");
+                        if (employees.Count == 0)
+                        {
+                            Console.WriteLine("No employees yet registered.");
+                        }
+                        else
+                        {
+                            for (int i = 0; i < employees.Count; i++)
+                            {
+
+                                employees[i].PrintEmployeeDetails();
+                            }
+                        }
+                        break;
+                    case "9":
+                        runProgram = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid command. Try again");
+                        break;
+
+                }
+
+
+
+
+
+                Console.WriteLine(" ");
+            } while (runProgram);
+            
+
+            void AddEmployee()
+            {
+                start:
+                Console.WriteLine(" ");
+                Console.WriteLine("Add Employee: ");
+                Console.Write("Name: ");
+#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
+                string name = Console.ReadLine();
+#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
+                if (name == null || name == "")
+                {
+                    Console.WriteLine("Invalid name");
+                    goto start;
+                }
+                else
+                {
+                    float wage;
+
+                    try
+                    {
+                        Console.Write("Wage: ");
+#pragma warning disable CS8604 // Possible null reference argument.
+                        wage = float.Parse(Console.ReadLine());
+#pragma warning restore CS8604 // Possible null reference argument.
+
+
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Invalid wage");
+                        Console.WriteLine(ex.Message);
+                        goto start;
+                    }
+
+
+                    employees.Add(new Employee(name, wage));
+                    Console.WriteLine(" ");
+                    Console.WriteLine("Employee registered.");
+                    Console.WriteLine(" ");
+
+                }
+            }
+
 
 
         }
