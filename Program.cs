dfce1473ab4a79062fb2a11007590c3eb0189ea7
Zhark1561 <Simon.Haglund@futuregames.nu>
2024-09-10 14:04:18 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Exercise1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.ReadLine();
+
+
+        }
+    }
+}
